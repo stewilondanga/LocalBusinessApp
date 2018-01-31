2e6e6171afd9d246030db8539b7e1a8d2e0a6ddc
Stewart ilondanga <stilondanga@gmail.com>
2018-01-31 10:32:27 +0300

    add app/src/main/java/io/github/stewilondanga/localbusinessapp/MoodDialogFragment.java

--- app/src/main/java/io/github/stewilondanga/localbusinessapp/MoodDialogFragment.java
+++ app/src/main/java/io/github/stewilondanga/localbusinessapp/MoodDialogFragment.java
@@ -1,8 +1,47 @@
 package io.github.stewilondanga.localbusinessapp;
 
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 /**
  * Created by stewart on 1/31/18.
  */
 
-public class MoodDialogFragment {
+public class MoodDialogFragment extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
+
+        Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
+        Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
+
+        RadioGroup surveyRadioGroup = (RadioGroup) rootView.findViewById(R.id.moodRadioGroup);
+        int selectedId = surveyRadioGroup.getCheckedRadioButtonId();
+        final RadioButton selectedRadioButton = (RadioButton) rootView.findViewById(selectedId);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("testing output", selectedRadioButton.getText().toString();
+                dismiss();
+            }
+        });
+
+        return rootView;
+    }
 }
